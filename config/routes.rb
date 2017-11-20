884e33afa02ad32bc081227443aab243854595e9
Ahmedali89 <ahmedali89@berkeley.edu>
2017-11-19 23:21:52 -0800

    uptodate

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
-  get '/users', to: 'users#index', as: 'users'
-  get '/users/:id', to: 'users#show', as: 'user'
+  #get '/users', to: 'users#index', as: 'users'
+  #get '/users/:id', to: 'users#show', as: 'user'
+  resources :users
 
 end
