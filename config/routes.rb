4627e682cc454d89a453cb6c0aeccc9a173cd429
Carson Trinh <carsontrinh@gmail.com>
2017-11-23 01:46:27 -0800

    Revert to 838c11479e396f6c734f45ba5d4421392ffaedc3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
-  #get '/users', to: 'users#index', as: 'users'
-  #get '/users/:id', to: 'users#show', as: 'user'
-  resources :users
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
 
 end
